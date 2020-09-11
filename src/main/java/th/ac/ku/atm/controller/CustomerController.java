bd674d0a0a7a0b8c18547fafd6c2cf30f43cc8fc
Nutthanich Narphromar <monaprom134@gmail.com>
2020-09-11 15:34:23 +0700

    Change dummy customer to object

--- src/main/java/th/ac/ku/atm/controller/CustomerController.java
+++ src/main/java/th/ac/ku/atm/controller/CustomerController.java
@@ -10,13 +10,13 @@ import java.util.List;
 @Controller
 public class CustomerController {
 
-    private List<String> customers = new ArrayList<>();
+    private List<Customer> customers = new ArrayList<>();
 
     @RequestMapping("/customer")
     public String getCustomerPage(Model model) {
-        customers.add("Peter");
-        customers.add("Nancy");
-        customers.add("Rick");
+        customers.add(new Customer(1, "Peter", 1234));
+        customers.add(new Customer(2, "Nancy", 2345));
+        customers.add(new Customer(3, "Rick", 3456));
 
         model.addAttribute("allCustomers", customers);
         return "customer";
