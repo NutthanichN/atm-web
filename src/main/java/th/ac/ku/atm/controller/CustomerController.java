778919edf90c119df4929c41d656fe7e36bcd356
Nutthanich Narphromar <monaprom134@gmail.com>
2020-09-11 16:18:59 +0700

    Refactor customer

--- src/main/java/th/ac/ku/atm/controller/CustomerController.java
+++ src/main/java/th/ac/ku/atm/controller/CustomerController.java
@@ -6,29 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import th.ac.ku.atm.model.Customer;
+import th.ac.ku.atm.service.CustomerService;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
-    private List<Customer> customers = new ArrayList<>();
+    private CustomerService customerService;
 
-    @GetMapping("/customer")
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
     public String getCustomerPage(Model model) {
-//        customers.add(new Customer(1, "Peter", 1234));
-//        customers.add(new Customer(2, "Nancy", 2345));
-//        customers.add(new Customer(3, "Rick", 3456));
-
-        model.addAttribute("allCustomers", customers);
+        model.addAttribute("allCustomers", customerService.getCustomers());
         return "customer";
     }
 
-    @PostMapping("/customer")
+    @PostMapping
     public String registerCustomer(@ModelAttribute Customer customer, Model model) {
-        customers.add(customer);
-        model.addAttribute("allCustomers", customers);
+        customerService.createCustomer(customer);
+        model.addAttribute("allCustomers", customerService.getCustomers());
         return "redirect:customer";
     }
 }
